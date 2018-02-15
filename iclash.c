055bfc4bd14f21a908b775278e783d6a0e281700
Twix1996 <36512755+Twix1996@users.noreply.github.com>
2018-02-15 19:07:42 +0100

    Update iclash.c

--- iclash.c
+++ iclash.c
@@ -1,10 +1,35 @@
-// test
+//
+//  main.c
+//  Droper
+//
+//  Created by DROPER on 15/02/2018.
+//  Copyright © 2018 DROPER. All rights reserved.
+//
 
-
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    return 0
+    int a[22][22];
+    int i,j;
+
+    for(i = 0 ; i <= 22 ; i++)
+    {
+        for(j = 0 ; j <= 22 ; j++)
+        {
+            if( i == 0 || i == 22)
+            {
+                a[i][j] = '_';
+            }
+            if( j == 0 || j == 22)
+            {
+                a[i][j] = '/';
+            }
+        }
+        printf("\n");
+    }
+    printf(a);
+    return 0;
+
 }
