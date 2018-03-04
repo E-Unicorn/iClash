877673c1ca5361049ab6e30a9fab7e325809a081
Stephen AP <36512484+EverAged@users.noreply.github.com>
2018-03-04 22:03:11 +0100

    Update iclash.c

--- iclash.c
+++ iclash.c
@@ -85,19 +85,3 @@ void set_color(int text_color,int background_color)
         SetConsoleTextAttribute(H,background_color*16+text_color);
 }
 
-// void init_obs(int taille_x, int taille_y)
-// {
-//     int k;
-//     for(k=1; k<=nombre_mines; k++)              //On distribu les obstacles dans le tableau aléatoirement
-//     {
-//         i=aleat(taille_x); j=aleat(taille_y);
-//         if(tableau[i][j] != -3)                 //La valeur -3 représente l'obstacle
-//         {
-//             tableau[i][j] = -3;
-//         }
-//         else
-//         {
-//             k--;
-//         }
-//     }
-// }
