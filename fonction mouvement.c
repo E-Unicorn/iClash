cd033fc557c02f70acb733d557fa9a5896a679d6
Antoine <antoine.didier@efrei.net>
2018-03-07 16:05:06 +0100

    maj

--- fonction mouvement.c
+++ fonction mouvement.c
@@ -9,43 +9,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//fct qui définie aléatoirement la position de l'obs
+int aleat(int maxi) // La fonction va choisir une coordonnée de i et j au hasard
+{
+    return 1 + (rand() % maxi);
+}
+
+
 
 //la fonction sera un mouvement completement random pour la demo
-
-
 int mouvement_random(int **map, int x, int y)
 {
-  int choix;
+  int choix, a;
+  a = aleat(6);
   choix = rand() %;
   switch(choix)
   {
     case 1:
-      if (map[x+1]==-3) {return map[x][y];}
+      if (map[x+a]==-3) {return map[x][y];}
       else
       {
-        map[x][y] = map[x + 1][y];
+        map[x][y] = map[x + a][y];
       }
       break;
     case 2:
-      if (map[x-1]==-3) {return map[x][y];}
+      if (map[x-a]==-3) {return map[x][y];}
       else
       {
-        map[x][y] = map[x - 1][y];
+        map[x][y] = map[x - a][y];
       }
       break;
     case 3:
-      if (map[y+1]==-3) {return map[x][y];}
+      if (map[y+a]==-3) {return map[x][y];}
       else
       {
-        map[x][y] = map[x][y + 1];
+        map[x][y] = map[x][y + a];
       }
       break;
     case 4:
-      if (map[y-1]==0) {return map[x][y];}
+      if (map[y-a]==0) {return map[x][y];}
       {
-        map[x][y] = map[x][y - 1];
+        map[x][y] = map[x][y - a];
       }
       break;
   }
   return map[x][y];
+}
+
+int main()
+{
+  mouvement_random(map,x,y);
+  return 0;
 }
